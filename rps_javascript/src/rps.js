de3ac4ae27b3380844e983ac9d29a9dd98df2c5f
munemo <john.chimbani@outlook.com>
2020-09-24 00:02:19 +0200

    Moifying rps.js file with if statement

--- rps_javascript/src/rps.js
+++ rps_javascript/src/rps.js
@@ -1,14 +1,27 @@
-function choiceGenerator() {
+function game() {
+  //const player1Choice = choices[Math.floor(Math.random() * choices.length)];
+          
+  var inputVal = document.getElementById("player1").value;
+  var player1Points = 0;
+  var player2Points = 0;
+  var player1Input = prompt("Player 1, please make a choice");
+  var player2Input = prompt("Player 2, please make a choice");
 
-choices = ["Rock", "Papper", "Scissors"];
-const player1Choice = choices[Math.floor(Math.random() * choices.length)];
-const player2Choice = choices[Math.floor(Math.random() * choices.length)];
   
-
-document.getElementById("player1").innerHTML = player1Choice;
-document.getElementById("player2").innerHTML = player2Choice;
-
+  while (player2Points < 3) {
   
-
+    
+    if (player1Input == "rock" && player2Input == "rock") {
+      alert("Its a tie!");
+    } else if (player1Input == "rock" && player2Input == "scissors") {
+      player1Points++;
+    } else if (player1Input == "rock" && player2Input == "papper") {
+      player1Points++;
+    } else if (player2Input == "rock" && player1Input == "scissors") {
+      player2Points++;
+    } else if (player2Input == "rock" && player1Input == "papper") {
+      player2Points++;
+    }
+  
+  }
 }
-
